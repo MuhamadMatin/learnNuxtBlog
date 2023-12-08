e41b9ef126ecaa70d0ad693ae522e3bcf7a7cd1e
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-12-08 13:12:22 +0700

    add new paragraf

--- components/content/Btn.vue
+++ components/content/Btn.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NuxtLink to="https://myportowithnuxt.netlify.app" target="blank">
-      <button class="py-2 px-4 bg-white rounded-2xl border hover:bg-[#2c3e50] hover:text-white hover:duration-300">
+      <button class="py-2 px-4 bg-white rounded-2xl border hover:bg-[#000103] hover:text-white hover:duration-300">
         <slot></slot>
       </button>
     </NuxtLink>
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -8,17 +8,17 @@
       <!-- header -->
       <header>
         <div class="text-center p-5">
-          <h1 class="text-4xl font-semibold">
+          <h1 class="text-2xl md:text-4xl font-semibold">
             {{ doc.title }}
           </h1>
           <div class="text-gray-500 text-sm m-2">
             {{ doc.publishedAt }}
           </div>
         </div>
-        <img :src="doc.thumbnail" alt="image thumnail" class="w-full h-full rounded-2xl object-cover" />
+        <img :src="doc.thumbnail" alt="image thumnail" class="w-full md:h-80 rounded-md md:rounded-2xl md:object-cover" />
       </header>
       <!-- main -->
-      <article class="py-3 px-2 text-sm lg:text-md md:py-4 md:px-3">
+      <article class="py-3 px-2 lg:text-md md:py-4 md:px-2">
         <ContentRenderer :value="doc" />
       </article>
     </ContentDoc>
